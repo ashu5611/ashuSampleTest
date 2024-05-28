45f6ee86c1a1c27b86d35878420f58475d8677ae
Ashutosh <ashutosh.gupta@inter.ikea.com>
2024-05-28 22:39:25 +0200

    fix

--- src/main/java/com/example/ashu/ashutest/BookRepo.java
+++ src/main/java/com/example/ashu/ashutest/BookRepo.java
@@ -1,9 +1,11 @@
 package com.example.ashu.ashutest;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BookRepo extends JpaRepository<Book, Long> {
     List<Book> findByTitle(String title);
 }
