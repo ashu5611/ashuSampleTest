e771dfe2ffe6f78274ae1e329097ff26542997b8
Ashutosh <ashutosh.gupta@inter.ikea.com>
2024-05-29 10:10:36 +0200

    Added

--- src/main/java/com/example/ashu/ashutest/AshutestApplication.java
+++ src/main/java/com/example/ashu/ashutest/AshutestApplication.java
@@ -1,15 +1,12 @@
 package com.example.ashu.ashutest;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +31,11 @@ public class AshutestApplication {
     public void create(@RequestParam(value = "id", defaultValue = "World") int id) {
         bookRepo.save(new Book(id, "Book 1"));
     }
+
+	@GetMapping("/create")
+	public List<Book> getAll() {
+		return bookRepo.findAll();
+	}
 
 
 
